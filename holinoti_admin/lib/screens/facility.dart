ec78bffbeb38c380d4f30bbadaf888ef000dffe3
도우진 <dwj1996@naver.com>
2020-06-07 07:31:08 +0900

    Implement temporary holiday input popup

--- holinoti_admin/lib/screens/facility.dart
+++ holinoti_admin/lib/screens/facility.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:holinoti_admin/bloc/facility_bloc.dart';
 import 'package:holinoti_admin/bloc/facility_input_bloc.dart';
 import 'package:holinoti_admin/constants/strings.dart' as Strings;
@@ -7,6 +9,7 @@ import 'package:holinoti_admin/screens/widgets/facility/facility_card.dart';
 import 'package:holinoti_admin/screens/widgets/facility/input_card.dart';
 import 'package:holinoti_admin/screens/widgets/global/lower_half.dart';
 import 'package:holinoti_admin/screens/widgets/global/upper_half.dart';
+import 'package:intl/intl.dart';
 
 // ignore: must_be_immutable
 class FacilityPage extends StatelessWidget {
@@ -74,23 +77,117 @@ class FacilityPage extends StatelessWidget {
                     PopupMenuButton<int>(
                       icon: const Icon(Icons.more_vert),
                       onSelected: (int index) async {
+                        Future<bool> _onWillPop(void onDismiss()) async {
+                          if (onDismiss != null) onDismiss();
+                          return true;
+                        }
+                        onConfirm() {
+                          
+                        }
                         switch (index) {
                           case 0:
-                            _facilityBloc.updateMode = true;
+                            showCupertinoDialog(
+                                context: context,
+                                builder: (context)=>WillPopScope(
+                                  onWillPop: () async => _onWillPop(onConfirm),
+                                  child: PlatformAlertDialog(
+                                    title: Text(Strings.GlobalPage.ALERT_TITLE),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        TextField(
+                                          decoration: InputDecoration(
+                                              labelText: "휴업 사유"
+                                          ),
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("휴업일:"),
+                                            StreamBuilder<DateTime>(
+                                              initialData: _facilityBloc.holidayStart,
+                                              stream: _facilityBloc.holidayStartStream,
+                                              builder: (context, snapshot) {
+                                                assert(snapshot != null && snapshot.data != null);
+                                                return FlatButton(
+                                                    child: Text(DateFormat('yyyy-MM-dd').format(snapshot.data)),
+                                                    onPressed: () async =>
+                                                        _facilityBloc.holidayStart = await showDatePicker(
+                                                          context: context,
+                                                          initialDate: DateTime.now(),
+                                                          firstDate: DateTime.now(),
+                                                          lastDate: DateTime(
+                                                              DateTime.now().year+1
+                                                          ),
+                                                        )
+                                                );
+                                              }
+                                            ),
+                                            Text("~"),
+                                            StreamBuilder<DateTime>(
+                                                initialData: _facilityBloc.holidayEnd,
+                                                stream: _facilityBloc.holidayEndStream,
+                                              builder: (context, snapshot) {
+                                                assert(snapshot != null && snapshot.data != null);
+                                                return FlatButton(
+                                                    child: Text(DateFormat('yyyy-MM-dd').format(snapshot.data)),
+                                                    onPressed: () async =>
+                                                        _facilityBloc.holidayEnd = await showDatePicker(
+                                                          context: context,
+                                                          initialDate: DateTime.now(),
+                                                          firstDate: DateTime.now(),
+                                                          lastDate: DateTime(
+                                                              DateTime.now().year+3
+                                                          ),
+                                                        )
+                                                );
+                                              }
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    actions: <Widget>[
+                                      PlatformButton(
+                                        androidFlat: (BuildContext context) => MaterialFlatButtonData(
+                                          child: Text(Strings.GlobalPage.BUTTON_CONFIRM),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            if (onConfirm != null) onConfirm();
+                                          },
+                                        ),
+                                        ios: (BuildContext context) => CupertinoButtonData(
+                                          child: Text(Strings.GlobalPage.BUTTON_CONFIRM),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            if (onConfirm != null) onConfirm();
+                                          },
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                            );
                             break;
                           case 1:
+                            _facilityBloc.updateMode = true;
+                            break;
+                          case 2:
                             await _facilityInputBloc.deleteFacility(context);
                             break;
                         }
                       },
                       itemBuilder: (BuildContext context) =>
                           <PopupMenuEntry<int>>[
+                            const PopupMenuItem<int>(
+                              value: 0,
+                              child: Text("휴업일 관리"),
+                            ),
+                            const PopupMenuItem<int>(
+                              value: 1,
+                              child: Text(Strings.GlobalPage.EDIT),
+                            ),
                         const PopupMenuItem<int>(
-                          value: 0,
-                          child: Text(Strings.GlobalPage.EDIT),
-                        ),
-                        const PopupMenuItem<int>(
-                          value: 1,
+                          value: 2,
                           child: Text(Strings.GlobalPage.DELETE),
                         ),
                       ],
---
+++ holinoti_admin/lib/screens/temp_holiday.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class TempHolidayPage extends StatelessWidget {
+  // TODO 다이얼로그 대신 이 페이지를 구현
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
